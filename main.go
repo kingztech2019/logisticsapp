2980b436f23d6bb03581800f621bedceff1af11f
Oluwajuwon <kingzmentechnology@gmail.com>
2022-01-03 15:47:29 +0100

    modify the port

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 
 	"github.com/kingztech2019/9jarider/database"
 	//"github.com/kingztech2019/9jarider/models"
@@ -12,6 +16,11 @@ import (
 func main() {
 	 
 	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+	port:=os.Getenv("PORT")
 	
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
@@ -21,5 +30,5 @@ func main() {
 //   MailChan:= make(chan models.MailData)
 //    defer close(MailChan)
 
- app.Listen(":3000")
+ app.Listen(port)
 }
